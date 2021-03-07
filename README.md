496bc6d72edc7542e6a135efddc563168838d5be
John-Mancini <mancinijohn5988@gmail.com>
2021-03-06 20:04:19 -0500

    added comments and read me

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Coding-Quiz
+
+sources
+
+https://www.w3schools.com/jsref/prop_style_display.asp
+https://stackoverflow.com/
+https://github.com/jamierachael/Code-Quiz
+https://www.youtube.com/watch?v=pQr4O1OITJo&t=471s&ab_channel=CodingNepal
+https://www.youtube.com/watch?v=WUBhpSRS_fk&ab_channel=CodingNepal
+
+This a coding quiz with a timer that subtracts time for every question wrong!
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Gathering HTML elements for Dom manipulation
+// Gathering HTML elements for Dom manipulation I get all my html elements by useing document.getElementById to call on all my html classes.
 var quiz = document.getElementById("quiz");
 var gradesEl = document.getElementById("grade");
 var finalScoreEl = document.getElementById("finalScore");
@@ -19,7 +19,7 @@ var buttonB = document.getElementById("b");
 var buttonC = document.getElementById("c");
 var buttonD = document.getElementById("d");
 
-// Quiz question object
+// This is where i create my quizQuestions variable making nall my questions objects with strings that state the question and possible answer choices and the correct answer.
 var quizQuestions = [{
     question:"1. An array can hold up to __________ elements?",
     choiceA:"3428372837",
@@ -60,71 +60,71 @@ var quizQuestions = [{
     choiceD:"Steve Jobs",
     correctAnswer:"c"},
 ];
-// Other global variables
+// Other variables that i created so that i can get create a timer fucntion and a function that checks my questions and answers.
 var finalQuestionIndex = quizQuestions.length;
 var currentQuestionIndex = 0;
 var timeLeft=100;
 var timerInterval;
 var score = 0;
-var correct;
+var correctAnswer;
 
 
 // This button starts the quiz
-startQuizButton.addEventListener("click",startQuiz);
+startQuizButton.addEventListener("click",startQuiz); // i declare startQuizButton and add an eventlistener to listen for a left click on the mouse which triggers my startQuiz function on line 90
 
 // This function cycles through the object array containing the quiz questions to generate the questions and answers.
-function generateQuizQuestion(){
-    gameoverSec.style.display = "none";
-    if (currentQuestionIndex === finalQuestionIndex){
+function generateQuizQuestion(){ //declare function generateQuizQuestion
+    gameoverSec.style.display = "none"; // use style.display = "none" to hide the gameoverSection linked to line 35 in the html.
+    if (currentQuestionIndex === finalQuestionIndex){ // if currentQuestionIndex is equal to the final question in my objects then js will return showscore which is the final score linked to lines 108.
         return showScore();
     } 
-    var currentQuestion = quizQuestions[currentQuestionIndex];
-    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
-    buttonA.innerHTML = currentQuestion.choiceA;
-    buttonB.innerHTML = currentQuestion.choiceB;
-    buttonC.innerHTML = currentQuestion.choiceC;
-    buttonD.innerHTML = currentQuestion.choiceD;
+    var currentQuestion = quizQuestions[currentQuestionIndex]; // create a variable currentQuestion and set it to my quizquestions which links this part to lines 27-31 of my html
+    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>"; //links to line 27
+    buttonA.innerHTML = currentQuestion.choiceA; //links to line 28
+    buttonB.innerHTML = currentQuestion.choiceB;//links to line 29
+    buttonC.innerHTML = currentQuestion.choiceC;//links to line 30
+    buttonD.innerHTML = currentQuestion.choiceD;//links to line 31
 };
 
 // Start Quiz function starts the TimeRanges, hides the start button, and displays the first quiz question.
-function startQuiz(){
-    gameoverSec.style.display = "none";
-    startQuizSec.style.display = "none";
-    generateQuizQuestion();
+function startQuiz(){ //create startQuiz function
+    gameoverSec.style.display = "none"; //hides game over box
+    startQuizSec.style.display = "none"; //hides start button
+    generateQuizQuestion(); //calls on generateQuizQuestions function
 
     //Timer
-    timerInterval = setInterval(function() {
-        timeLeft--;
-        quizTimer.textContent = "Time left: " + timeLeft;
+    timerInterval = setInterval(function() { //timerInterval uses js internal setInterval to start a timer countdown using a function.
+        timeLeft--; //this line calls my variable timeLeft and subtracts it by 1 using -- 
+        quizTimer.textContent = "Time left: " + timeLeft; //use the js intertnal script textContent to add a string that will say time left: + the amount of seconds left as the timer counts down linked to line 26 in html
     
-        if(timeLeft === 0) {
-          clearInterval(timerInterval);
+        if(timeLeft === 0) {    // if statement that means if timeLeft is STRICTLY EQUAL to 0 then js will clear the interval of (timerInterval) and return the show score function
+          clearInterval(timerInterval); // meaning is the timer hits 0 before the quiz is actually finished then the showscore function will appear
           showScore();
         }
-      }, 1000);
-    quiz.style.display = "block";
+      }, 1000); // this 1000 is miliseconds of countdown ensuring that the countdown is going down by 1 second everysecond when the quiz has started.
+    quiz.style.display = "block";  //uses style.display ="block" so that my quiz element is displayed as a block-level element
 }
 // This function is the end page screen that displays your score after either completeing the quiz or upon timer run out
-function showScore(){
-    quiz.style.display = "none"
-    gameoverSec.style.display = "flex";
-    clearInterval(timerInterval);
-    highscoreName.value = "";
-    finalScoreEl.innerHTML = "You got " + score + " out of " + quizQuestions.length + " correct!";
+function showScore(){ //declare showScore function
+    quiz.style.display = "none" //hides the quiz block 
+    gameoverSec.style.display = "flex"; // this makes may gameoverSec variable display as a block-level flex box
+    clearInterval(timerInterval); // clears the timer off the screen 
+    highscoreName.value = ""; // sets the highscore value to nothing so that the user can enter in initials / name
+    finalScoreEl.innerHTML = "You got " + score + " out of " + quizQuestions.length + " correct!"; // links to line 36 in html and displays "you got (your score) out of (how many questions are in the quizQuestion by using .length) correct". .length is 5 because i have 5 questions. 
 }
 
 // submit button that saves the arrays of the highscore already saved in local storage
 // as well as pushing the new user name and score into the array we are saving in local storage. Then it runs the function to show high scores.
-submitScoreBtn.addEventListener("click", function highscore(){
+submitScoreBtn.addEventListener("click", function highscore(){  //add an event lisnter to submitScorebtn variable from line 15 to listen for mouse left click and then go to highscore page by making the function
     
     
-    if(highscoreName.value === "") {
+    if(highscoreName.value === "") {         // if the highscore value is STRICTLY EQUAL  to nothing then the browser will alert the user that the initials block cannot be left blank.
         alert("Initials cannot be blank");
         return false;
     }else{
-        var savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
-        var currentUser = highscoreName.value.trim();
-        var currentHighscore = {
+        var savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];  //use json.parse to turn my into data which is stored in the local storage by getting the scores from savedHighscores
+        var currentUser = highscoreName.value.trim();    // these 4 lines make it so that when the highscore page is up it will display name and the username the person enter 
+        var currentHighscore = {   
             name : currentUser,
             score : score
         };
@@ -135,7 +135,7 @@ submitScoreBtn.addEventListener("click", function highscore(){
         endGameBtns.style.display = "flex";
         
         savedHighscores.push(currentHighscore);
-        localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
+        localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));  // takes the localStorage and uses JSON.stringify to make the data back into a string 
         generateHighscores();
 
     }
@@ -144,15 +144,15 @@ submitScoreBtn.addEventListener("click", function highscore(){
 
 // This function clears the list for the high scores and generates a new high score list from local storage
 function generateHighscores(){
-    highscoreUsername.innerHTML = "";
+    highscoreUsername.innerHTML = ""; //links to line
     highscoreDisplayScore.innerHTML = "";
     var highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
-    for (i=0; i<highscores.length; i++){
-        var newNameSpan = document.createElement("li");
+    for (i=0; i<highscores.length; i++){ //for loop that continues to add every highscore until the highscore page is cleared
+        var newNameSpan = document.createElement("li"); //creates li for score and name
         var newScoreSpan = document.createElement("li");
         newNameSpan.textContent = highscores[i].name;
         newScoreSpan.textContent = highscores[i].score;
-        highscoreUsername.appendChild(newNameSpan);
+        highscoreUsername.appendChild(newNameSpan);  //appends the lis
         highscoreDisplayScore.appendChild(newScoreSpan);
     }
 }
@@ -187,15 +187,17 @@ function replayQuiz(){
 
 // This functions chesks if the answer chosen is right or wrong
 function checkAnswer(answer){
-    correct = quizQuestions[currentQuestionIndex].correctAnswer;
+    var currentQuestion=quizQuestions[currentQuestionIndex]
+    var correctAnswer=currentQuestion.correctAnswer
+    // correctAnswer = quizQuestions[currentQuestionIndex].correctAnswer;
 
-    if (answer === correct && currentQuestionIndex !== finalQuestionIndex){
+    if (answer === correctAnswer && currentQuestionIndex !== finalQuestionIndex){
         score++;
         alert("That Is Correct!");
         currentQuestionIndex++;
         generateQuizQuestion();
         //display in the grades div that the answer is correct.
-    }else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex){
+    }else if (answer !== correctAnswer && currentQuestionIndex !== finalQuestionIndex){
         alert("That Is Incorrect.")
         currentQuestionIndex++;
         timeLeft -=15; //takes away 15 secs per wrong answer
